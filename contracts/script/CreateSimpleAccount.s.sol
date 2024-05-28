af044e75b904d13ba11fa3eb119c982a7b8e9a11
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2024-05-28 13:55:33 +0400

    init a script to create a simple account

---
+++ contracts/script/CreateSimpleAccount.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.25;
+
+import { Script, console } from "forge-std/Script.sol";
+import { SimpleAccountFactory } from "../src/account/SimpleAccountFactory.sol";
+
+contract CreateSimpleAccountScript is Script {
+
+  SimpleAccountFactory public simpleSccountFactory;
+
+  function setUp() public {
+    uint256 deployerPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+    simpleSccountFactory = new SimpleAccountFactory();
+     vm.stopBroadcast();
+  }
+
+  function run() public {
+    uint256 accountOwnerPrivateKey = vm.envUint("ACCOUNT_OWNER_PRIVATE_KEY");
+    vm.startBroadcast(accountOwnerPrivateKey);
+    address accountOwner = vm.addr(accountOwnerPrivateKey);
+    address account = simpleSccountFactory.createAccount(accountOwner, "salt1");
+    console.log("Account 1 address: ", account);
+    console.log("Owner address: ", accountOwner);
+  }
+}
+
